b1cb643321615fb2353fdde50a06edd1d2f8e02e
00anuyh <ovo5959@gmail.com>
2025-09-01 17:35:02 +0900

    로그인 기능 추가
    로그인 기능 추가

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Install from './install';
+import StarRating from './components/StarRating';
+import { Routes, Route } from 'react-router-dom';
+
+import Cart from './cart/Cart';
+import Login from './login/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={
+          <>
+            <h1>PWA 테스트</h1>
+            <Install />
+            <StarRating itemId={1} />
+            <StarRating itemId={2} />
+            <StarRating itemId={3} />
+          </>
+        }></Route>
+        <Route path='/Cart' element={<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
