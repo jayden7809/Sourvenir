46cc08053226f92d7b456beb4c0fc0f459f0330a
yujeong-yuyu <uju4320@gmail.com>
2025-09-01 17:51:34 +0900

    장바구니 내용추가-555
    장바구니 내용추가-555

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Cart() {
-    return <h1>장바구니페이지</h1>
+    return (
+        <>
+            <h1>장바구니페이지</h1>
+            <p>안녕하세요</p>
+        </>
+    )
 }
 
 export default Cart;
